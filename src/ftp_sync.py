8ee841692cf53f2b2da9b3b0adbd4554663b985a
Owen Jarrett <owen.t.jarrett@gmail.com>
2017-10-03 01:34:01 -0700

    Add retries to FTP sync

--- src/ftp_sync.py
+++ src/ftp_sync.py
@@ -1,17 +1,33 @@
+import ftplib
 from ftplib import FTP
 from ftp_downloader import FtpDownloader
 from ftp_crawler import FtpCrawler
 
 class FtpSync:
-    def __init__(self, host, dest_dir):
+    DEFAULT_FTP_TIMEOUT = 5
+    DEFAULT_FTP_CONNECT_RETRIES = 10
+    def __init__(self, host, dest_dir, ftp_timeout=DEFAULT_FTP_TIMEOUT, ftp_connect_retries=DEFAULT_FTP_CONNECT_RETRIES):
         self.host = host
         self.dest_dir = dest_dir
         # Lazy init
         self.connection = None
+        self.ftp_timeout = ftp_timeout
+        self.ftp_connect_retries = ftp_connect_retries
+
+    def __try_ftp_connect(self):
+        attempt = 0
+        while attempt < self.ftp_connect_retries:
+            attempt = attempt + 1
+            try:
+                connection = FTP(host=self.host, timeout=self.ftp_timeout)
+                return connection
+            except ftplib.all_errors as e:
+                print("Timed out connecting to %s, retrying. Exception thrown:%s", self.host, str(e))
+                if attempt == self.ftp_connect_retries: raise e
 
     def sync_path(self, path):
         if self.connection is None:
-            self.connection = FTP(host=self.host)
+            self.connection = self.__try_ftp_connect()
 
         ftp_crawler = FtpCrawler(ftplib=self.connection, base_path=path)
         ftp_downloader = FtpDownloader(ftplib=self.connection,
