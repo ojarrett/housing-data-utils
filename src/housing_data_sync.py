070dc8fc20bba97faaac9d4c8bc5a4c3e83cc0cd
Owen Jarrett <owen.t.jarrett@gmail.com>
2017-10-03 01:34:55 -0700

    Move housing data sync to src directory

--- src/housing_data_sync.py
+++ src/housing_data_sync.py
@@ -1,10 +1,11 @@
-from src.ftp_sync import FtpSync
+from ftp_sync import FtpSync
 import sys
 import os
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
         print("Usage: python housing_data_sync.py <ftp-server> <base-path>")
+        sys.exit(1)
 
     ftp_sync = FtpSync(host=sys.argv[1], dest_dir=(os.curdir + "/data"))
-    ftp_sync.sync_path(path=argv[2])
+    ftp_sync.sync_path(path=sys.argv[2])
