da7abbe1ca3b1748c4014094c0e528b274ff20a8
Owen Jarrett <owen.t.jarrett@gmail.com>
2017-10-06 05:03:45 -0700

    Move CSV header processing to csv_utils, add option to build housing DF from multiple files

--- src/csv_utils.py
+++ src/csv_utils.py
@@ -1,6 +1,19 @@
 CSV_HEADER_LINES = [0, 1]
 CSV_DELIMITER = ","
 
+def get_header_lines_from_file(file_path):
+    header = None
+
+    with open(file_path, "r") as input_file:
+        header_lines = [input_file.readline().strip().split(CSV_DELIMITER) for line in CSV_HEADER_LINES]
+        header = merge_header_lines(header_lines)
+
+    if header is None:
+        print("Failed to extract header lines. Exiting...")
+        exit(1)
+
+    return header
+
 def merge_header_lines(header_lines):
     short_line = header_lines[0]
     long_line = header_lines[1]
--- src/housing_df.py
+++ src/housing_df.py
@@ -1,28 +1,45 @@
 import pandas as pd
 import src.csv_utils as csv_utils
+import os
 
 class HousingDFBuilder:
 
     def __init__(self):
         self.csv = None
         self.filter_fields = None
+        self.csv_prefix = None
+        self.csv_suffix = None
+        self.csv_dir = None
 
     def set_csv(self, csv):
         self.csv = csv
 
+    def set_csv_prefix(self, csv_prefix):
+        self.csv_prefix = csv_prefix
+
+    def set_csv_suffix(self, csv_suffix):
+        self.csv_suffix = csv_suffix
+
+    def set_csv_dir(self, csv_dir):
+        self.csv_dir = csv_dir
+
     def set_filter_fields(self, filter_fields):
         self.filter_fields = filter_fields
 
+    def __file_matches(self, file_name):
+        result = True
+
+        if self.csv_prefix is not None:
+            if not file_name.startswith(self.csv_prefix):
+                return False
+
+        if self.csv_suffix is not None:
+            if not file_name.endswith(self.csv_suffix):
+                return False
+
     def build(self):
-        header = None
-        with open(self.csv, "r") as input_file:
-            header_lines = [input_file.readline().strip().split(csv_utils.CSV_DELIMITER) for line in csv_utils.CSV_HEADER_LINES]
 
-            header = csv_utils.merge_header_lines(header_lines)
+        file_list = [self.csv_dir + "/" + file_name for file_name in os.listdir(self.csv_dir) if self.__file_matches(file_name)] if self.csv is None else [self.csv]
 
-        if header is None:
-            print("Failed to extract header lines. Exiting...")
-            exit(1)
-
-        temp_df = pd.read_csv(self.csv, header=None, skiprows=csv_utils.CSV_HEADER_LINES, names=header)
+        temp_df = pd.concat([pd.read_csv(file_name, header=None, skiprows=csv_utils.CSV_HEADER_LINES, names=csv_utils.get_header_lines_from_file(file_name)) for file_name in file_list])
         return temp_df if self.filter_fields is None else temp_df[self.filter_fields]
