1e8e5ebea81444f504ef591ad5c2ea131df97cd4
Owen Jarrett <owen.t.jarrett@gmail.com>
2020-04-15 08:28:09 -0700

    Fix housing DF utils smoke test

--- test/test_housing_df_utils.py
+++ test/test_housing_df_utils.py
@@ -2,4 +2,4 @@ from housing_df_utils import get_housing_df_for_region
 
 def test_get_housing_df_for_region():
     df = get_housing_df_for_region('we')
-    assert len(df[df['Place Name']] == 'San Francisco') > 0
+    assert len(df[df['Place Name'] == 'San Francisco']) > 0
