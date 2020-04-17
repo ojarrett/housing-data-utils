23fcc531550b475b7bcd3b07da4ee2e516fa1334
Owen Jarrett <owen.t.jarrett@gmail.com>
2020-04-17 13:47:15 -0700

    Shorten housing DF util test by using small CSV data directory

--- src/housing_df_utils.py
+++ src/housing_df_utils.py
@@ -7,7 +7,7 @@ DATA_DIR = "data"
 class InvalidInputException(Exception):
     pass
 
-def get_housing_df_for_region(region):
+def get_housing_df_for_region(region, csv_dir=DATA_DIR):
     if not region in VALID_REGIONS:
         raise InvalidInputException("Unrecognized region: " + str(region))
 
@@ -15,7 +15,7 @@ def get_housing_df_for_region(region):
 
     housing_dfb.set_csv_prefix(region)
     housing_dfb.set_csv_suffix(CURRENT_MONTH_CSV_SUFFIX)
-    housing_dfb.set_csv_dir(DATA_DIR)
+    housing_dfb.set_csv_dir(csv_dir)
     housing_dfb.set_before_date(210001)
     housing_dfb.set_after_date(199912)
 
--- test/test_housing_df_utils.py
+++ test/test_housing_df_utils.py
@@ -1,5 +1,5 @@
 from housing_df_utils import get_housing_df_for_region
 
 def test_get_housing_df_for_region():
-    df = get_housing_df_for_region('we')
+    df = get_housing_df_for_region('we', csv_dir='test/data')
     assert len(df[df['Place Name'] == 'San Francisco']) > 0
